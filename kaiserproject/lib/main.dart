818406169a6c54c3e2adf67e960001d74e22a385
Kalie Tong <kalietong@gmail.com>
2020-04-24 23:10:59 -0700

    finished the visuals for 'contact us' page

--- kaiserproject/lib/main.dart
+++ kaiserproject/lib/main.dart
@@ -5,16 +5,18 @@ import 'package:kaiserproject/start.dart';
 import 'package:kaiserproject/sign_in.dart';
 import 'package:kaiserproject/sign_up.dart';
 import 'package:kaiserproject/new_chat.dart';
+import 'package:kaiserproject/contact_us.dart';
 
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/sign in',// this is for testing purposes, inital route should be the start file later on
+  initialRoute: '/sign in',// this is for testing purposes, initial route should be the start file later on
   routes: {
     '/starter':(context) => Start(),
     '/sign in': (context) => SignIn(),
     '/sign up':(context) => SignUp(),
     '/new chat':(context) => NewChat(),
     '/survey':(context)=> Survey(),
+    '/contact us':(context) => ContactUs(),
   },
 ));
 
